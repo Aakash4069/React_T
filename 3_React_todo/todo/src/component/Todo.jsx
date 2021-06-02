6f2a783cc67ca9ee06f05e2c68def1c48cb92769
aakash4069 <aakash4069@gmail.com>
2021-06-02 19:26:35 +0530

    Todo

---
+++ 3_React_todo/todo/src/component/Todo.jsx
@@ -0,0 +1,14 @@
+//es7
+//rcc
+
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>
+                Todo App
+            </div>
+        )
+    }
+}
